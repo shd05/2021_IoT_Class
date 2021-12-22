1c0db2e26ebad77e0d70cc5f8cadebbc241e7dbf
Hyeon Do Shin <84717745+shd05@users.noreply.github.com>
2021-12-22 10:19:21 +0900

    Add files via upload

---
+++ 03_high_level_sensor/dht11 temperature.py
@@ -0,0 +1,18 @@
+import time
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT11
+DHT_PIN = 12
+
+try:
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT_PIN)
+        if humidity is not None and temperature is not None:
+            print('Temperature=%.1f*, Humidity=%.1f%%' % (temperature, humidity))
+        else:
+            print('Read Error')
+               
+
+finally:
+     print("Bye Bye")
+        
